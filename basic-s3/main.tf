92d1fc1e3fff516d72933fa473ac8b6b93a8af80
Withney Guilherme <wguilhermecriativo@gmail.com>
2024-12-03 20:44:54 -0300

    feat: add basic s3 specification

---
+++ basic-s3/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = "1.9.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+provider "aws" {
+  region  = "us-east-1"
+  profile = "terraform-study-wg-dev"
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket#private-bucket-w-tags
+resource "aws_s3_bucket" "my-test-bucket" {
+  bucket = "wguilherme.dev.terraform.study.bucket-000000001"
+
+  tags = {
+    Name        = "Bucket created by Terraform"
+    Environment = "dev"
+    Managedby   = "terraform"
+  }
+}
