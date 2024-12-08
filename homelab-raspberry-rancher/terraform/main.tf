05637918849a7932a76dde59c7cfc2bd9d2d1fdf
Withney Guilherme <wguilhermecriativo@gmail.com>
2024-12-07 21:21:03 -0300

    feat: split in roles, rancher and kubernetes cluster, add new machine forcluster, update makefile command to select host actions to execute in playbook

--- homelab-raspberry-rancher/terraform/main.tf
+++ homelab-raspberry-rancher/terraform/main.tf
@@ -32,10 +32,31 @@ resource "aws_instance" "rancher" {
   }
 }
 
+resource "aws_instance" "k3s" {
+  ami           = "ami-07ee04759daf109de" // ubuntu 22.04 arm
+  instance_type = "t4g.large"
+  key_name      = "study"
+
+  vpc_security_group_ids      = ["sg-0897f9f4f1033d53a"]
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_size = 64
+    volume_type = "gp2"
+  }
+
+  tags = {
+    Name = "k3s1-arm64"
+  }
+}
+
 resource "local_file" "ansible_inventory" {
   content  = <<-EOT
     [rancher]
     ${aws_instance.rancher.public_ip}
+
+    [k3s]
+    ${aws_instance.k3s.public_ip}
   EOT
   filename = "../ansible/inventory.ini"
 }
